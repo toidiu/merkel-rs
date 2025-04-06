e322d75d5c8c794c4b1e6f1f955a279fd2ba66e5
Apoorv Kothari <akothari@cloudflare.com>
2025-04-06 12:32:41 -0700

    impl contains

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![allow(unused)]
 
 use boring::hash::hash;
+use boring::hash::DigestBytes;
 use boring::hash::Hasher;
 use boring::hash::MessageDigest;
 use std::cell::RefCell;
@@ -35,7 +36,7 @@ impl Node {
 
 struct Tree {
     root: Node,
-    data_len: usize,
+    elem_cnt: usize,
     depth: usize,
 }
 
@@ -49,7 +50,7 @@ impl Tree {
 
         Tree {
             root,
-            data_len,
+            elem_cnt: data_len,
             depth,
         }
     }
@@ -64,29 +65,48 @@ impl Tree {
 
     // The position of the original data
     pub fn contains(&self, data: String, pos: usize) -> Result<bool, ()> {
-        if pos + 1 > self.data_len {
+        if pos + 1 > self.elem_cnt {
             return Err(());
         }
 
         // data should be one of the leaf nodes. calculate the hash to compare
-        let data_hash = hash(MessageDigest::sha256(), data.as_bytes()).unwrap();
+        // let data_hash = hash(MessageDigest::sha256(), data.as_bytes()).unwrap();
+        let data_hash = Self::hash_data(data.as_bytes());
         let data_hash = data_hash.as_ref();
 
-        let root = self.root();
-        if &root.hash == data_hash {
+        let mut cmp_node = Box::new(self.root().clone());
+        if &cmp_node.hash == data_hash {
             return Ok(true);
         }
 
-        // based on the position we need to traverse the tree down to the leaf node
-        while root.left.borrow().is_some() || root.right.borrow().is_some() {
-            // TODO
-            // data_len / 2
-            // if in lower half then traverse left, else right
+        let mut left = 0;
+        let mut right = self.elem_cnt - 1;
 
-            // compare the hash and return true if eq
+        // based on the position we need to traverse the tree down to the leaf node.
+        // continue until we are at the leaf nodes
+        while cmp_node.left.borrow().is_some() || cmp_node.right.borrow().is_some() {
+            let search_space = right - left;
+            if search_space == 0 {
+                // we are done. compare and return
+                unreachable!("loop should exit");
+            }
+
+            let mid_idx = search_space / 2;
+            if pos <= mid_idx {
+                // search left
+                cmp_node = cmp_node.left.clone().borrow().clone().unwrap();
+            } else {
+                // search right
+                cmp_node = cmp_node.right.clone().borrow().clone().unwrap();
+            }
         }
 
-        Ok(false)
+        assert_eq!(left, right);
+        if &cmp_node.hash == data_hash {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 
     // Must be called with data of non-zero length
@@ -99,9 +119,7 @@ impl Tree {
         let mut layer: Vec<Node> = data
             .iter()
             .map(|data| {
-                let hash = hash(MessageDigest::sha256(), data.as_bytes())
-                    .unwrap()
-                    .to_vec();
+                let hash = Self::hash_data(data.as_bytes()).to_vec();
 
                 Node::new(hash, RefCell::new(None), RefCell::new(None))
             })
@@ -148,6 +166,10 @@ impl Tree {
         h.update(&right.hash).unwrap();
         let res = h.finish().unwrap();
         res.to_vec()
+    }
+
+    fn hash_data(data: &[u8]) -> DigestBytes {
+        hash(MessageDigest::sha256(), data).unwrap()
     }
 }
 
