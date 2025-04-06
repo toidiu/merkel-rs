a56d60e28768420957af5ba30af859cecc01bac6
Apoorv Kothari <akothari@cloudflare.com>
2025-04-05 18:09:56 -0700

    construct leaf nodes. wip to build tree

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
+use boring::hash::hash;
+use boring::hash::MessageDigest;
 use std::cell::RefCell;
 use std::hash::Hash;
 
@@ -9,9 +11,19 @@ fn main() {
 }
 
 struct Node {
-    data: String,
-    left: RefCell<Option<Box<Node>>>,
-    right: RefCell<Option<Box<Node>>>,
+    hash: Vec<u8>,
+    left: Option<RefCell<Box<Node>>>,
+    right: Option<RefCell<Box<Node>>>,
+}
+
+impl Node {
+    fn new(
+        hash: Vec<u8>,
+        left: Option<RefCell<Box<Node>>>,
+        right: Option<RefCell<Box<Node>>>,
+    ) -> Self {
+        Node { hash, left, right }
+    }
 }
 
 struct Tree {
@@ -20,17 +32,45 @@ struct Tree {
 }
 
 impl Tree {
-    fn new_with_data(data: &[String]) -> Self {
-        todo!()
-        // Tree {}
+    /// Must be called with data of non-zero length
+    pub fn new_with_data(data: &[String]) -> Self {
+        assert!(!data.is_empty());
+
+        let data_len = data.len();
+        let root = Self::build_tree(data);
+
+        Tree { root, data_len }
     }
 
-    fn root(&self) -> Node {
+    pub fn root(&self) -> Node {
         todo!()
     }
 
-    fn contains(&self, data: String) -> bool {
+    pub fn contains(&self, data: String) -> bool {
         todo!()
+    }
+
+    // Must be called with data of non-zero length
+    fn build_tree(data: &[String]) -> Node {
+        assert!(!data.is_empty());
+
+        let root = Node::new(vec![], None, None);
+
+        // Construct the leaf nodes first
+        let mut layer: Vec<Node> = data
+            .iter()
+            .map(|data| {
+                let hash = hash(MessageDigest::sha256(), data.as_bytes())
+                    .unwrap()
+                    .to_vec();
+
+                Node::new(hash, None, None)
+            })
+            .collect();
+
+        // Iter over data and construct layers from the leaf up to the root
+
+        root
     }
 }
 
